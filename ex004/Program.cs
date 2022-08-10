e0642f1919fbdb3fa1bab9c51d0ac1676ddad3e8
unknown <igor.d85@mail.ru>
2022-08-10 12:22:02 +0300

    на вход 3 числа и выдача максимального

---
+++ ex004/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число А:");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B:");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("Введите число C:");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max ) max = numberB;
+if (numberC > max ) max = numberC;
+
+Console.Write(max);
