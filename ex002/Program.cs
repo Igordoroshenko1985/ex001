69e7faaa75d0407d1c28b113a5f6291b82dfa287
unknown <igor.d85@mail.ru>
2022-08-10 09:07:24 +0300

    нашли числа от -n do n

---
+++ ex002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+int i = 0;
+
+i = i - (number + 1);
+
+while (i < number)
+{
+    i += 1;
+    Console.Write(i + " ");
+}
+Console.WriteLine();
