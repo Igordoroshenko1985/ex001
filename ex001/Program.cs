fcde74a691d8f8c911faa44116379674530a0c1d
unknown <igor.d85@mail.ru>
2022-08-10 08:04:22 +0300

    сравнили является ли в квадратом а

---
+++ ex001/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Clear();
+Console.Write("ВВедите число А");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число Б");
+int numberB = int.Parse(Console.ReadLine()); 
+int sqr = numberA * numberA;
+  if (sqr == numberB)
+{
+    Console.Write("да");
+}
+else
+{
+    Console.Write("нет");
+}
+ 
