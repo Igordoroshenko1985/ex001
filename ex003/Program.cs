7e56a01e5a104985f31ab937c0d4754161c6070a
unknown <igor.d85@mail.ru>
2022-08-10 11:34:12 +0300

    2 числа показать какое больше какое меньше и вывести

---
+++ ex003/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число А:");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B:");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("numberA - max");
+    Console.WriteLine("numberB - min");
+}
+else 
+{
+    Console.WriteLine("numberB - max");
+    Console.WriteLine("numberA - min");
+}
